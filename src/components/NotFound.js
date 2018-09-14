8761fb0640a2731eb439c7d07a08e384a19a741e
wagnxx <2362999724@qq.com>
2018-09-14 23:44:05 +0800

    补充404页面和wallet页面

---
+++ src/components/NotFound.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 19848 on 2018/9/14.
+ */
+import React from 'react';
+
+
+export default class NotFound extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign:"center",paddingTop:"10%",fontWeight:"800"}}>
+               <h3 style={{ fontWeight:"800"}}>404 Not Found</h3>
+            </div>
+        );
+    }
+}
+
+ 
